11d4003cf5365c07f3ad9ab2eedb6eecaa290023
garnich <garnich@tut.by>
2018-03-14 17:49:55 +0300

    task second zeroes for 100%

--- src/index.js
+++ src/index.js
@@ -3,52 +3,62 @@ module.exports = function getZerosCount(number, base) {
 
   var arr =[];
   var ii = 2;
-              while (ii <= base)
-              {
-                  if (base % ii == 0)
+
+  var temp = number;
+      for( ii; ii <= base; ii++){
+            //  while (ii <= base)
+                var z = 0;
+                while (base % ii == 0)  //кол-во множителей
                   {
+                                          z++;
                    //console.log(ii);
-                      base = base / ii;
+                      base = Math.floor(base / ii);
                       arr.push(ii);
-                  }
-                  else
-                  {
-                      ii++;
-                  }
-              }//console.log(arr);
-              var t= arr.length-1;
-              console.log(t + " t number");
-              var y = 1;
-              if(t >= 0){
-                for (var h = t; h >= 0; h--){
 
-                if(arr[arr.length-1]==arr[h-1]){
-                  console.log(t);
-                  console.log(arr[h] + " massive");
-                  console.log(arr[h-1] + " massive");
-                  y=y+1;
-                  console.log(y + " this is y");
-                }
-              }};
-  var ad = ii;
+                  }
+
+              //console.log(arr);
+            //   var t= arr.length-1;
+            // //  console.log(t + " t number");
+            //   var y = 1;
+            //   if(t >= 0){
+            //     for (var h = t; h >= 0; h--){
+            //
+            //     if(arr[arr.length-1]==arr[h-1]){
+            //     //  console.log(t);
+            //     //  console.log(arr[h] + " massive");
+            //     //  console.log(arr[h-1] + " massive");
+            //       y=y+1;
+            //     //  console.log(y + " this is y");
+            //     }
+            //   }};
+  //var ad = ii;
+  var ad = 0;
+  var out = number;
    //console.log(ad + " составные")
-
-   var m, f, t, q;
-   var v = 0;
-   if(ad == 2){
-     m =  Math.floor(Math.log(number))}else{
-       m =  Math.floor(Math.log(number)) / Math.floor(Math.log(ad));
-     }
-    q = Math.floor(m);
-   //console.log(q + " типа ответ");
-   var t = 0;
-   for(var i=1; i<=q; i++){
-     f = Math.pow(ad,i);
-     v = v + Math.floor(number/f);
-   }
+while(out/ii > 0){
+  ad = ad +  Math.floor(out/ii); //сумма делителей
+  out = Math.floor(out/ii); // цельный остаток от деления
+}
+   // var m, f, t, q;
+   // var v = 0;
+   // if(ad == base){
+   //   m =  Math.floor(Math.log(number))}else{
+   //     m =  Math.floor(Math.log(number)) / Math.floor(Math.log(ad));
+   //   }
+   //  q = Math.floor(m);
+   // //console.log(q + " типа ответ");
+   // var t = 0;
+   // for(var i=1; i<=q; i++){
+   //
+   //   v = v + Math.floor(number/ii);
+   //    f = Math.pow(ad,i);
+   // }
   //console.log(v + "  answer");
-
-
-  return Math.floor(v/y);
+if(temp > ad/z){
+  temp = ad/z;
+}
+}
+  return Math.floor(temp);
   // your implementation
 }
